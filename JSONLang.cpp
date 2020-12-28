8a67cee638b436a9fd5c347b54899978ce169de4
konvol <konstantinosvolakakis@gmail.com>
2020-12-28 17:58:54 +0200

    Basic overloading for printing. Support for number, string and boolean(maybe).

--- JSONLang.cpp
+++ JSONLang.cpp
@@ -3,10 +3,16 @@
 
 PROGRAM_BEGIN
 
-start();
+JSON(temp_string) = STRING("first attempt")
+JSON(temp_integer) = NUMBER(3)
+JSON(temp_double) = NUMBER(3.14)
+JSON(key_temp) = KEY(test)
 
-JSON(temp_string) = STRING("first attempt");
-
-cout << temp_string.getValue() << endl;
+PRINT "\n\n===program output===\n"
+PRINT temp_string
+PRINT temp_integer
+PRINT temp_double
+PRINT "key: "
+PRINT key_temp
 
 PROGRAM_END
--- JSONLang.h
+++ JSONLang.h
@@ -6,10 +6,13 @@
 #include <fstream>
 #include <typeinfo>
 
-#define PROGRAM_BEGIN int main(){
-#define PROGRAM_END  return 0;} 
-#define JSON(temp) ;JSON_String temp 
-#define STRING(value) *new JSON_String(value)   
+#define PROGRAM_BEGIN int main(){ string tmp;
+#define PROGRAM_END  ; return 0;} 
+#define JSON(temp) ;JSON_val temp 
+#define STRING(value) *new JSON_val((string)value) 
+#define NUMBER(value) *new JSON_val((double)value) 
+#define KEY(value) STRING(#value) //# is used to stringify value
+#define PRINT ; cout << 
 
 
 using namespace std;
@@ -19,30 +22,22 @@ typedef enum JSON_Type{
     INTEGER,
     DOUBLE,
     BOOLEAN,
-    NULL_TYPE,
+    NIL,
     OBJECT,
     ARRAY
 }JSON_Type;
 
 
-class JSON_String{
+class JSON_val{
     private:
-        JSON_Type type = STRING;
-        string value;
+        JSON_Type type;
+        string strValue;
+        double numValue;
+        bool boolValue;
     public:
         string  temp;
-    JSON_String(string value){
-        this->value = value;
-    }
-    
-    string getValue(){
-        return this->value;
-    }
 
-    void setValue(string newValue){
-        this->value = newValue;
-    }
-
+    //common methods for every value type
     JSON_Type getType(){
         return this->type;
     }
@@ -51,10 +46,70 @@ class JSON_String{
         this->type = newType;
     }
 
+    //basic methods regarding strings    
+    JSON_val(string value){
+        this->type = STRING;
+        this->strValue = value;
+        cout << "creating string " << this->strValue << endl;
+    }
+    string getStrValue(){
+        return this->strValue;
+    }
+
+    void setStrValue(string newValue){
+        this->strValue = newValue;
+    }
+
+    //basic methods regarding numbers (doubles and integers)
+    JSON_val(int value){
+        this->type = INTEGER;
+        this->numValue = value;
+        cout << "creating integer " << this->numValue << endl;
+    }
+
+    JSON_val(double value){
+        this->type = DOUBLE;
+        this->numValue = value;
+        cout << "creating double " << this->numValue << endl;
+    }
+
+    double getNumValue(){
+        return this->numValue;
+    }
+
+    void setNumValue(double newValue){
+        this->numValue = newValue;
+    }
+
+    //-----NEEDS TO BE CHECKED..NOT SURE IF RIGHT-----//
+    //basic methods regarding bool valus (true/false)
+    JSON_val(bool value){
+        this->type = BOOLEAN;
+        this->boolValue = value;
+    }
+
+    double getBoolValue(){
+        return this->boolValue;
+    }
+
+    void setNumValue(bool newValue){
+        this->boolValue = newValue;
+    }
+    //-----END NEEDS TO BE CHECKED-----//
+
+    friend ostream &operator<<(ostream  &os, JSON_val &json){
+        switch(json.getType()){
+            case STRING:
+                cout << json.getStrValue() << endl;
+                break;
+            case INTEGER:
+            case DOUBLE:
+                cout << json.getNumValue() << endl;
+                break;
+            case BOOLEAN:
+                cout << json.getBoolValue() << endl;
+                break;
+        }
+    }
+
 };
-
-
-
-void start(){
-    std::cout << "Let's begin" << std::endl;
-}
