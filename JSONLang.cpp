26b72151aa69bb551ad39c747de8fd6727374d0e
konvol <konstantinosvolakakis@gmail.com>
2020-12-29 22:31:47 +0200

    Key passed to each pair of object.

--- JSONLang.cpp
+++ JSONLang.cpp
@@ -6,20 +6,22 @@ PROGRAM_BEGIN
 JSON(temp_string) = STRING("first attempt")
 JSON(temp_integer) = NUMBER(3)
 JSON(temp_double) = NUMBER(3.14)
-JSON(obj) = OBJECT { 
+JSON(outer) = OBJECT { 
     KEY(key) : STRING("value"),
     KEY(key2) : STRING("value2"),
     KEY(number): NUMBER(32.6),
-    KEY(Obj) : OBJECT {
+    KEY(inner) : OBJECT {
         KEY(p) : STRING("fuck yeah")
     }
 }
+
+//JSON(arr) = ARRAY[NUMBER(2), STRING("3")]
 
 PRINT "\n\n===program output===\n"
 PRINT temp_string
 PRINT temp_integer
 PRINT temp_double
-PRINT obj
+PRINT outer
 
 PROGRAM_END
 
--- JSONLang.h
+++ JSONLang.h
@@ -6,24 +6,25 @@
 #include <fstream>
 #include <typeinfo>
 
-#define PROGRAM_BEGIN int main(){ string tmp; string  temp_key;
+#define PROGRAM_BEGIN int main(){ string tmp; string temp_key = "key"; 
 #define PROGRAM_END  ; return 0;} 
-#define JSON(temp) ; JSON_val temp 
-#define STRING(value) JSON_val((string)value) 
-#define NUMBER(value) JSON_val((double)value)
+#define JSON(temp) ; ::json_name=#temp; JSON_val temp
+#define STRING(value) JSON_val((string)value, temp_key) 
+#define NUMBER(value) JSON_val((double)value, temp_key)
 #define OBJECT  *new JSON_val
+#define ARRAY JSON_val("array")
 /*
  * # is used to stringify value
  * trickiest so far (commit 3)
  * assign true value to expression so that using expr1 ? expr2 : expr3 leads to calling both expr1 and expr3
  */ 
-#define KEY(value) JSON_val(#value) = false ? true
+#define KEY(value) (((temp_key = #value) == #value) && (cout << "key: " << temp_key << endl )  && false) ? true
 #define PRINT ; cout << 
 
 
 using namespace std;
 int tabs = 0;
-
+string json_name = "obj";
 typedef enum JSON_Type{
     STRING,
     INTEGER,
@@ -34,6 +35,10 @@ typedef enum JSON_Type{
     ARR
 }JSON_Type;
 
+string getJsonName(){return ::json_name;}
+
+//TODO: check if it's possible to keep different types of values using templates
+//TODO; maybe use map to keep (key, value) pairs
 class JSON_val{
     private:
         JSON_Type type;
@@ -63,7 +68,8 @@ class JSON_val{
     }
 
     //basic methods regarding strings    
-    JSON_val(string value){
+    JSON_val(string value, string key){
+        this->key = key;
         this->type = STRING;
         this->strValue = value;
         cout << "creating string " << this->strValue << endl;
@@ -77,13 +83,15 @@ class JSON_val{
     }
 
     //basic methods regarding numbers (doubles and integers)
-    JSON_val(int value){
+    JSON_val(int value, string key){
+        this->key = key;
         this->type = INTEGER;
         this->numValue = value;
         cout << "creating integer " << this->numValue << endl;
     }
 
-    JSON_val(double value){
+    JSON_val(double value, string key){
+        this->key = key;
         this->type = DOUBLE;
         this->numValue = value;
         cout << "creating double " << this->numValue << endl;
@@ -118,7 +126,8 @@ class JSON_val{
         vector<JSON_val>::iterator obj_iterator;
         obj_iterator = this->object.begin();
         obj_iterator = this->object.insert(obj_iterator, list);
-        this->type = OBJ; 
+        this->type = OBJ;  
+        this->key = getJsonName();  
     }
 
     vector<JSON_val> getObject(){
@@ -127,6 +136,7 @@ class JSON_val{
 
 
     friend ostream &operator<<(ostream  &os, JSON_val &json){
+        cout << "\"" << json.getKey() <<"\" : ";
         switch(json.getType()){
             case STRING:
                 cout << "\"" <<json.getStrValue() << "\"" << endl;
@@ -144,7 +154,7 @@ class JSON_val{
                 //cout << "size: " << json.getObject().size() << endl;
                 for(int i = 0; i < json.getObject().size(); i++){
                     for(int j = 0; j<tabs; j++) cout << "   ";
-                    cout << i <<"-> type: " << json.getObject()[i].getType() << ": ";
+                    //cout << i <<"-> type: " << json.getObject()[i].getType() << ": ";
                     PRINT json.getObject()[i];
                 }
                 tabs-=2;
@@ -160,4 +170,16 @@ class JSON_val{
         return *value;
     }
 
+    /*JSON_val &operator,(JSON_val value){
+        this->object.push_back(value);
+        return value;
+    }
+
+    JSON_val &operator[](JSON_val value){
+        cout << "in array making " << value.getType() << endl;
+        return value;
+    }
+
+    JSON_val operator[](JSON_val Head){return Head;};
+    JSON_val operator[](JSON_val *Head){return Head;};*/
 };
